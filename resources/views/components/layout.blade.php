2eb90f5f348282a7bbc9e876476240955c61af1c
muhammadderic <muhammad.donny.e@gmail.com>
2024-03-31 07:17:24 +0700

    Build create page and its functionality

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,6 +9,11 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased dark:bg-black dark:text-white/50">
+      @session("message")
+        <div class="success-message">
+          {{ session("message") }}
+        </div>
+      @endsession
       {{ $slot }}
     </body>
 </html>
